23a10c62f9204ce9766c1a5b2088d078b91247ab
Yune <yuneuzuki@gmail.com>
2024-04-20 23:24:39 -0300

    Started UI Development

--- src/App.js
+++ src/App.js
@@ -1,8 +1,263 @@
 import "./style.css";
+import { MdReplay } from "react-icons/md";
 
 function App() {
   return (
-    <div className="view">
+    <div className="background">
+      <div className="main-view">
+        <div className="assembly">
+          <div className="assembly-sidecollumn"></div>
+          <div className="assembly-content overflow">
+            <div className="title">
+              <div className="assembly-toprow"></div>
+              <h1>ASSEMBLY</h1>
+            </div>
+            <br/>
+            <div className="category">
+                <h2>UNIT</h2>
+                <div className="entry-container">
+                  <div className="entry selectable">
+                    <h3>R-ARM UNIT</h3>
+                    <div className="row">
+                      <span>PART NAME</span>
+                      <div className="collumn">
+                        <span className="weight-or-en">WEIGHT: 1000</span>
+                        <span className="weight-or-en">EN LOAD: 1000</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="single-rando-button selectable">
+                    <MdReplay size={37} color="#FFF"/>
+                  </div>
+                </div>
+                <div className="entry-container">
+                  <div className="entry selectable">
+                    <h3>L-ARM UNIT</h3>
+                    <div className="row">
+                      <span>PART NAME</span>
+                      <div className="collumn">
+                        <span className="weight-or-en">WEIGHT: 1000</span>
+                        <span className="weight-or-en">EN LOAD: 1000</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="single-rando-button selectable">
+                    <MdReplay size={37} color="#FFF"/>
+                  </div>
+                </div>
+                <div className="entry-container">
+                  <div className="entry selectable">
+                    <h3>R-BACK UNIT</h3>
+                    <div className="row">
+                      <span>PART NAME</span>
+                      <div className="collumn">
+                        <span className="weight-or-en">WEIGHT: 1000</span>
+                        <span className="weight-or-en">EN LOAD: 1000</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="single-rando-button selectable">
+                    <MdReplay size={37} color="#FFF"/>
+                  </div>
+                </div>
+                <div className="entry-container">
+                  <div className="entry selectable">
+                    <h3>L-BACK UNIT</h3>
+                    <div className="row">
+                      <span>PART NAME</span>
+                      <div className="collumn">
+                        <span className="weight-or-en">WEIGHT: 1000</span>
+                        <span className="weight-or-en">EN LOAD: 1000</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="single-rando-button selectable">
+                    <MdReplay size={37} color="#FFF"/>
+                  </div>
+                </div>
+            </div>
+            <br/>
+            <div className="category">
+                <h2>FRAME</h2>
+                <div className="entry-container">
+                  <div className="entry selectable">
+                    <h3>HEAD</h3>
+                    <div className="row">
+                      <span>PART NAME</span>
+                      <div className="collumn">
+                        <span className="weight-or-en">WEIGHT: 1000</span>
+                        <span className="weight-or-en">EN LOAD: 1000</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="single-rando-button selectable">
+                    <MdReplay size={37} color="#FFF"/>
+                  </div>
+                </div>
+                <div className="entry-container">
+                  <div className="entry selectable">
+                    <h3>CORE</h3>
+                    <div className="row">
+                      <span>PART NAME</span>
+                      <div className="collumn">
+                        <span className="weight-or-en">WEIGHT: 1000</span>
+                        <span className="weight-or-en">EN LOAD: 1000</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="single-rando-button selectable">
+                    <MdReplay size={37} color="#FFF"/>
+                  </div>
+                </div>
+                <div className="entry-container">
+                  <div className="entry selectable">
+                    <h3>ARMS</h3>
+                    <div className="row">
+                      <span>PART NAME</span>
+                      <div className="collumn">
+                        <span className="weight-or-en">WEIGHT: 1000</span>
+                        <span className="weight-or-en">EN LOAD: 1000</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="single-rando-button selectable">
+                    <MdReplay size={37} color="#FFF"/>
+                  </div>
+                </div>
+                <div className="entry-container">
+                  <div className="entry selectable">
+                    <h3>LEGS</h3>
+                    <div className="row">
+                      <span>PART NAME</span>
+                      <div className="collumn">
+                        <span className="weight-or-en">WEIGHT: 1000</span>
+                        <span className="weight-or-en">EN LOAD: 1000</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="single-rando-button selectable">
+                    <MdReplay size={37} color="#FFF"/>
+                  </div>
+                </div>
+            </div>
+            <br/>
+            <div className="category">
+                <h2>INNER</h2>
+                <div className="entry-container">
+                  <div className="entry selectable">
+                    <h3>BOOSTER</h3>
+                    <div className="row">
+                      <span>PART NAME</span>
+                      <div className="collumn">
+                        <span className="weight-or-en">WEIGHT: 1000</span>
+                        <span className="weight-or-en">EN LOAD: 1000</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="single-rando-button selectable">
+                    <MdReplay size={37} color="#FFF"/>
+                  </div>
+                </div>
+                <div className="entry-container">
+                  <div className="entry selectable">
+                    <h3>FCS</h3>
+                    <div className="row">
+                      <span>PART NAME</span>
+                      <div className="collumn">
+                        <span className="weight-or-en">WEIGHT: 1000</span>
+                        <span className="weight-or-en">EN LOAD: 1000</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="single-rando-button selectable">
+                    <MdReplay size={37} color="#FFF"/>
+                  </div>
+                </div>
+                <div className="entry-container">
+                  <div className="entry selectable">
+                    <h3>GENERATOR</h3>
+                    <div className="row">
+                      <span>PART NAME</span>
+                      <div className="collumn">
+                        <span className="weight-or-en">WEIGHT: 1000</span>
+                        <span className="weight-or-en">EN LOAD: 1000</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="single-rando-button selectable">
+                    <MdReplay size={37} color="#FFF"/>
+                  </div>
+                </div>
+            </div>
+            <br/>
+            <div className="category">
+                <h2>EXPANSION</h2>
+                <div className="entry-container">
+                  <div className="entry selectable">
+                    <h3>EXPANSION</h3>
+                    <div className="row">
+                      <span>PART NAME</span>
+                      <div className="collumn">
+                        <span className="weight-or-en">WEIGHT: 1000</span>
+                        <span className="weight-or-en">EN LOAD: 1000</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="single-rando-button selectable">
+                    <MdReplay size={37} color="#FFF"/>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="collumn-2">
+          <div className="randomizer-options">
+            <div className="collumn-2-toprow"></div>
+            <h1>RANDOMIZER</h1>
+            <br/>
+            <div className="category">
+              <h2>RANDOMIZE</h2>
+              <div className="entry-container">
+                <div className="entry selectable">
+                  <span>RANDOMIZE BUILD</span>
+                </div>
+                <div className="entry selectable">
+                  <span>RANDOMIZE MISSION</span>
+                </div>
+              </div>
+            </div>
+            <br/>
+            <div className="category">
+              <h2>OPTIONS</h2>
+              <div className="option">
+                <input type="checkbox" id="no-overweight"/>
+                <span>DISABLE OVERWEIGHT</span>
+              </div>
+              <div className="option">
+                <input type="checkbox" id="no-weapon"/>
+                <span>INCLUDES "NOTHING" IN WEAPON ROTATION</span>
+              </div>
+            </div>
+          </div>
+          <br/>
+          <br/>
+          <div className="randomizer-options">
+            <div className="collumn-2-toprow"></div>
+            <h1>PART STATS</h1>
+          </div>
+        </div>
+
+        <div className="ac-stats">
+          <div className="ac-stats-content">
+            <div className="collumn-3-toprow"></div>
+            <div className="">
+              <h1>AC STATS</h1>
+            </div>
+          </div>
+          <div className="assembly-sidecollumn"></div>
+        </div>
+      </div>
     </div>
   );
 }
